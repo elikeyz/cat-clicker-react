c4f52218bacca613e4487ae2e3d557b13fab857b
Elijah Enuem-Udogu <koppter.kom@gmail.com>
2018-10-15 21:44:27 +0100

    set the app header

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <h1>Cat Clicker</h1>
+            </div>
+        );
+    }
+}
+
+export default Header;
